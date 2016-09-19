ba40bdce33c9b5778d9578acd98fbcc19e51168d
Steven Woodhall <steveslaptops@hotmail.co.uk>
2016-09-19 23:53:30 +0100

    makes one single exe file

--- createEXE.py
+++ createEXE.py
@@ -1,4 +1,21 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['demo.py'])
+setup(    
+    name = "Not Ransomware...",
+    version = '1.0',
+    description = "Totally not bad.",
+    author = "...",
+    windows = [{'script': 'demo.py'}],
+    zipfile = None,
+    data_files=[],
+    options = {
+        'py2exe': {
+            'optimize':2, 
+            'bundle_files': 1, 
+            'compressed': True, 
+            'excludes':[],
+            'dll_excludes':['w9xpopen.exe']
+            }
+        }
+)
